983935c03002a4426e30ec0f81d4263f94f5b1d6
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-05-01 23:19:01 +0300

    finish

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,27 @@
+
 // Your tests here
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome function', () => {
+  test('returns true for palindromes', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+    expect(isPalindrome('Racecar')).toBe(true);
+  });
+
+  test('returns false for non-palindromes', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+  test('returns false for an empty string', () => {
+    expect(isPalindrome('')).toBe(false);
+  });
+
+  test('throws error for input with non-alphabetic characters', () => {
+    expect(() => isPalindrome('12321')).toThrow();
+  });
+
+  test('throws error for non-string input', () => {
+    expect(() => isPalindrome(12321)).toThrow();
+  });
+});
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,20 @@
-// Your code here
+function isPalindrome(str) {
+    if (typeof str !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    if (!/^[A-Za-z]*$/.test(str)) {
+      throw new Error('Input must contain only alphabetic characters');
+    }
+  
+    const alphanumericStr = str.toLowerCase();
+    
+    if (alphanumericStr.length === 0 || alphanumericStr.length === 1) {
+      return false;
+    }
+    
+    return alphanumericStr === alphanumericStr.split('').reverse().join('');
+  }
+  
+  module.exports = { isPalindrome };
+  
